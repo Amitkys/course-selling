b9a10c2ec10a3449ccda2825e8ddc3569b8a889b
KYs <amitkys59@gmail.com>
2024-11-20 02:25:58 +0530

    texting with tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
     <div>
       <Accordion type="single" collapsible>
         <AccordionItem value="item-1">
-          <AccordionTrigger>Is it accessible?</AccordionTrigger>
+          <AccordionTrigger className="text-4xl">Is it accessible?</AccordionTrigger>
           <AccordionContent>
             Yes. It adheres to the WAI-ARIA design pattern.
           </AccordionContent>
