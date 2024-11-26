17a0c61e5edf9d22f72772c0669e234f44f0e6fb
KYs <amitkys59@gmail.com>
2024-11-26 17:31:49 +0530

    UI structure created

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,11 +1,5 @@
-import { Label } from "@/components/ui/label"
-import { Switch } from "@/components/ui/switch"
+import { Button } from "@/components/ui/button"
 
-export default function SwitchDemo() {
-  return (
-    <div className="flex items-center space-x-2">
-      <Switch id="airplane-mode" />
-      <Label htmlFor="airplane-mode">Airplane Mode</Label>
-    </div>
-  )
+export default function ButtonDemo() {
+  return <Button>Button</Button>
 }
