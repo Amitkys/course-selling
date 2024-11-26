fb1fc18156f8f60fd171280343ca3b64d7088194
KYs <amitkys59@gmail.com>
2024-11-26 09:05:18 +0530

    created form for adding opinioin

---
+++ app/opinion/page.tsx
@@ -0,0 +1,13 @@
+export default function opinion() {
+    return (
+        <div>
+            <form action="">
+                <label htmlFor="statement">Write Statement</label> <br />
+                <textarea name="statement" id=""></textarea> <br />
+                <label htmlFor="teacher">select teacher</label> <br />
+                <input type="text" name="teacher" /> <br />
+            </form>
+        </div>
+
+    )
+}
