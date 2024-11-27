874cfb5116a9ab719f0140d799103e50a1bb9eb9
KYs <amitkys59@gmail.com>
2024-11-27 12:17:05 +0530

    got all data on component

--- app/feedback/page.tsx
+++ app/feedback/page.tsx
@@ -27,38 +27,41 @@ import { getPost } from "@/lib/action"
 
 export default async function CardWithForm() {
     const data = await getPost();
-    console.log('here is all post', data);
+    // console.log('here is all post', data);
     return (
-        <Card className="w-[350px]">
-            <CardHeader>
-                <div className="flex justify-between mb-2" >
-                    <CardTitle>Anonymouse(***)</CardTitle> {/* star(*) stands for roll number */}
-                    <CardTitle>3m ago</CardTitle>
-                </div>
-                <hr  />
-                <div className="flex justify-center">
-                    <CardTitle className="text-green-600">Teacher: Amit Kumar</CardTitle>
-                </div>
-                <hr />
-            </CardHeader>
-            <CardContent className="flex justify-center mb-5">
-                <CardDescription>Deploy your new project in one-click.</CardDescription>
-                <br />
-            </CardContent>
-            <CardFooter className="flex justify-around">
-                <button >
-                    <div className="flex flex-col justify-center">
-                        <ThumbUpOutlinedIcon></ThumbUpOutlinedIcon>
-                        <div>5</div>
+        <div>
+            <h3>Students Feedback</h3>
+            <Card className="w-[350px]">
+                <CardHeader>
+                    <div className="flex justify-between mb-2" >
+                        <CardTitle>Anonymouse(***)</CardTitle> {/* star(*) stands for roll number */}
+                        <CardTitle>3m ago</CardTitle>
                     </div>
-                </button>
-                <button>
-                    <div className="flex flex-col justify-center">
-                        <ThumbDownOutlinedIcon />
-                        <div>5</div>
+                    <hr />
+                    <div className="flex justify-center">
+                        <CardTitle className="text-green-600">Teacher: Amit Kumar</CardTitle>
                     </div>
-                </button>
-            </CardFooter>
-        </Card>
+                    <hr />
+                </CardHeader>
+                <CardContent className="flex justify-center mb-5">
+                    <CardDescription>Deploy your new project in one-click.</CardDescription>
+                    <br />
+                </CardContent>
+                <CardFooter className="flex justify-around">
+                    <button >
+                        <div className="flex flex-col justify-center">
+                            <ThumbUpOutlinedIcon></ThumbUpOutlinedIcon>
+                            <div>5</div>
+                        </div>
+                    </button>
+                    <button>
+                        <div className="flex flex-col justify-center">
+                            <ThumbDownOutlinedIcon />
+                            <div>5</div>
+                        </div>
+                    </button>
+                </CardFooter>
+            </Card>
+        </div>
     )
 }
--- lib/action.ts
+++ lib/action.ts
@@ -1,4 +1,4 @@
-// "use server";
+"use server";
 import prisma from "@/lib/db";
 import { Opinion } from "@/lib/types";
 import { addNewStudentType } from "@/lib/types";
