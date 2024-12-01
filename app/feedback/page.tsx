87ab5471ed1178917cbf71276d63e02a06964a54
KYs <amitkys59@gmail.com>
2024-12-01 21:27:58 +0530

    added comments for reading better user fetching

--- app/feedback/page.tsx
+++ app/feedback/page.tsx
@@ -3,12 +3,15 @@ import { getPost, handleDislike, handleLike } from "@/lib/action";
 import { getServerSession } from "next-auth";
 
 export default async function feedbackServer() {
+    // getting user info from loggedin session
     const session = await getServerSession();
     let data;
+    // if user is logged in: they can other name on post, and they can react
     if (session && session.user) {
-        data = await getPost(true);
+        data = await getPost(true); // by passing 'true' means getting post conditionally (user is authorized)
     }else{
-        data = await getPost(false);
+        // if user is not logged in: prevent them to react to other post
+        data = await getPost(false); // by passing 'false' means user is not authorized and getting post accordingly
     }
 
     return (
