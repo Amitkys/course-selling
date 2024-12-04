4d4cd8e4a73fc38baa130c20e73f8623990de6a4
KYs <amitkys59@gmail.com>
2024-12-04 23:31:20 +0530

    added db logic and page for adding a new user

--- app/adduser/page.tsx
+++ app/adduser/page.tsx
@@ -1,27 +1,31 @@
-// "use client";     
+"use client";     
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import { useToast } from "@/hooks/use-toast";
 import { addUser } from "@/lib/actions/user";
-import React from "react"
+import React, { useState } from "react"
 
 export default function PageAddUser() {
+    const {toast} = useToast();
+    const [errorMessage, setErrorMessage] = useState<string | null>(null);
     async function handleForm(formData: FormData) {
-        "use server";
-        
+        // "use server";
+
         const response = await addUser(formData);
+       if(response.rollError) setErrorMessage(response.message);
+       if(!response.message) toast({description: response.message});
     }
   return (
       <div className="grid w-full max-w-sm items-center gap-1.5 ml-3">
           <form action={handleForm} >
               <Label htmlFor="roll">Roll</Label>
               <Input className="mb-2" type="number" id="roll" name="rollNumber"></Input>
-              <Label htmlFor="email">Email</Label>
+              <Label htmlFor="text">Email</Label>
               <Input className="mb-2" type="email" id="email" placeholder="Email" name="email" />
               <p className="mt-2 text-sm text-destructive" role="alert" aria-live="polite">
-                  Email is invalidV
+                {errorMessage}
               </p>
-              {/* <FormDescription>Hello bhai</FormDescription> */}
               <Button type="submit">Add new kkkkkstudent</Button>
           </form>
       </div>
--- lib/actions/user.ts
+++ lib/actions/user.ts
@@ -3,6 +3,48 @@
 import prisma from "@/lib/db";
 import { z } from 'zod';
 
+const addUserSchema = z.object({
+    rollNumber: z.string(),
+    email: z.string().email({message: 'email is not valid'}),
+})
+
 export async function addUser(formData: FormData){
-    
+    const verifiedData = addUserSchema.safeParse({
+        rollNumber: formData.get('rollNumber'),
+        email: formData.get('email'),
+    })
+
+    if(!verifiedData.success){
+        const error = verifiedData.error.format();
+        // console.log(error.email?._errors[0]);
+        return { success: false,  message: 'Invalid Data was sent'};
+    }
+
+    const {rollNumber, email} = verifiedData.data;
+
+    try{
+        const isAlreadyExist = await prisma.emailWithRoll.findUnique({
+            where: {rollNumber},
+        });
+
+        if(isAlreadyExist){
+            return {rollError: true, message: `${rollNumber} is Associated with ${isAlreadyExist.email}`}
+        }
+
+        await prisma.emailWithRoll.create({
+            data: {
+                rollNumber,
+                email,
+            },
+        });
+        return {success: true, message: 'New user added.'};
+    }catch(e){
+        return {
+            success: false,
+            message: 'Internal error while adding a new user',
+            e // from catch
+        }
+    }
+
+
 }
