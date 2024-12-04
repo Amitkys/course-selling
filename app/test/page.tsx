dd0a346c5531e827541a320db6a1d768f9572aec
KYs <amitkys59@gmail.com>
2024-12-04 18:34:39 +0530

    function execuated at client side

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,11 +1,20 @@
 // "use client";
-// import { signIn  } from "next-auth/react"
-// export default function test() {
+export default function Test() {
+    async function kys(formData: FormData) {
+        "use server"
 
-//     return (
-//         <div>
-//         <button onClick={() => signIn('google')}>sing as google</button>
-//         </div>
-//     )
-// }
-
+        const rawFormData = {
+            name: formData.get('name'),
+        }
+        console.log(rawFormData);
+    }
+    return (
+        <div>
+        <form action={kys}>
+            <label htmlFor="name">Enter your name</label> <br />
+            <input type="text" name="name" id="name" /> <br />
+            <button type="submit">add</button>
+        </form>
+        </div>
+    )
+}
