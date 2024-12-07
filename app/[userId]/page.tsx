013079e10b9486d0795cac333833f0e5f3850c8d
KYs <amitkys59@gmail.com>
2024-12-08 00:08:25 +0530

    now accessing all the post created by user

--- app/[userId]/page.tsx
+++ app/[userId]/page.tsx
@@ -1,14 +1,17 @@
 import { getAllUserPosts } from "@/lib/actions/user";
-import { RenderAllPostOfUser } from "@/components/RenderAllPostOfUser";
+// import { RenderAllPostOfUser } from "@/components/RenderAllPostOfUser";
 
 
 export default async function User({ params }: { params: { userId: string } }) {
     const { userId } = params;
+    console.log(userId);
     const posts = await getAllUserPosts(userId);
+    console.log(posts)
 
     return (
         <div>
-            <RenderAllPostOfUser posts={posts} />
+            <div>hello</div>
+            {/* <RenderAllPostOfUser posts={posts} /> */}
         </div>
     );
 }
