871ec2de2b36ab567a699c1911f697a4ebdbe0c0
KYs <amitkys59@gmail.com>
2024-12-07 23:46:52 +0530

    fresh page with typescript is createdf for getting all post of user

--- components/RenderAllPostOfUser.tsx
+++ components/RenderAllPostOfUser.tsx
@@ -1,5 +1,12 @@
-export  function RenderAllPostOfUser({posts}){
-    return {
-
-    }
+import { OpinionDataTypeForUser } from "@/lib/types"
+export  function RenderAllPostOfUser({posts}: {posts: OpinionDataTypeForUser[]}){
+    return (
+        <div>
+            {posts.map((post: OpinionDataTypeForUser) => (
+                <div key={post.id}>
+                    <div>{post.author.name}</div>
+                </div>
+            ))}
+        </div>
+    )
 }
