48100c11c5601a7855d3db9ecc477fc7033478bd
KYs <amitkys59@gmail.com>
2024-12-08 23:49:37 +0530

    fix font issue

--- components/RenderAllPostOfUser.tsx
+++ components/RenderAllPostOfUser.tsx
@@ -28,7 +28,7 @@ import * as React from "react"
 // import { useToast } from "@/hooks/use-toast";
 import { poppin } from "@/components/fonts";
 import { inter } from "@/components/fonts";
-import { useSession } from "next-auth/react";
+// import { useSession } from "next-auth/react";
 
 // format date and time
 function formatDateTime(dateString: Date): string {
@@ -63,9 +63,9 @@ function formatNumber(value: number): string {
 
 
 export  function RenderAllPostOfUser({posts}: {posts: OpinionDataTypeForUser[]}){
-      const { data: session } = useSession();
+//       const { data: session } = useSession();
 
-  console.log("Session on the client:", session);
+//   console.log("Session on the client:", session);
     return (
         
         <div className="flex justify-center items-center bg-zinc-900">
--- components/fonts.tsx
+++ components/fonts.tsx
@@ -1,9 +1,9 @@
-import { Poppins} from "next/font/google";
-import { Inter} from "next/font/google";
+import { Poppins, Inter} from "next/font/google";
 
 export const poppin = Poppins({
     subsets: ['latin'], 
-    weight: '400'
+    weight: '400',
+    display: 'swap'
 });
 
 export const inter = Inter({
