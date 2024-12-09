36c655319ead0c26222411efb458046a844b08a7
KYs <amitkys59@gmail.com>
2024-12-09 21:59:24 +0530

    UI improvedmain

--- app/addteacher/page.tsx
+++ app/addteacher/page.tsx
@@ -1,22 +1,37 @@
+import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import Email from "next-auth/providers/email";
 
 export default function InputDemo() {
+    const handleForm = async (formData: FormData) => {
+        "use server";
+        
+        const data = {
+            email: formData.get('email')?.toString(),
+            message: formData.get('message')?.toString(),
+        }
+        console.log(data);
+    }
   return (
-      <form action="">
+      <form action={handleForm}>
           <div className="space-y-2">
               <Label htmlFor="input-02">
                   Required input <span className="text-destructive">*</span>
               </Label>
-              <Input id="input-02" placeholder="Email" type="email" required />
+              <Input id="input-02" placeholder="Email" name="email" type="email" required />
           </div>
+
+            {/* Optional field  */}
           <div className="mb-2 flex items-center justify-between gap-1">
               <Label htmlFor="input-04" className="leading-6">
                   Input with hint
               </Label>
               <span className="text-sm text-muted-foreground">Optional</span>
           </div>
-          <Input id="input-04" placeholder="Email" type="email" />
+          <Input  id="input-04" placeholder="Email" name="message" type="text" />
+
+          <Button type="submit" className="mt-2">Add Teacher</Button>
       </form>
   );
 }
